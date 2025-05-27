a527225d60a34c90d845565fb45901feeb4c33fa
Jaider-lopez <lopezjhonjaider949@gmail.com>
2025-05-27 18:55:04 -0500

    se intenta hacer la conexion con la base de datos

---
+++ conexion.php/mostrar.php
@@ -0,0 +1,43 @@
+<?php
+
+$inc = include("con_bd_tabla");
+if ($inc) { 
+    $consulta = "SELECT * FROM INCIDENTES_LA_CEJA";
+    $resultado = mysqli_query($conex,$consulta);
+    if ($resultado) {
+        while($row = $resultado->fetch_array()){
+            $ID = $row ["ID"];
+            $NOMBRE = $row ["NOMBRE"];
+            $TELEFONO = $row ["TELEFONO"];
+            $EVENTO = $row ["EVENTO"];
+            $FECHA = $row ["FECHA"];
+            $HORA_RECEPCION = $row ["HORA_RECEPCION"];
+            $HORA_ATENCION = $row ["HORA_ATENCION"];
+            $DIRECCION = $row ["DIRECCION"];
+            $DESCRIPCION = $row ["DESCRIPCION"];
+            $ENTIDAD = $row ["ENTIDAD"];
+            $OPERADOR_RESPONSABLE = $row ["OPERADOR_RESPONSABLE"];
+               }
+    }
+}
+
+            ?>
+
+            <div>
+               <h2><?php echo $NOMBRE; ?></h2>
+                <div>
+                    <>
+                        <b>ID: </b> <?php echo $ID; ?><br>
+                        <b>TELEFONO: </b> <?php echo $TELEFONO; ?> <br>
+                        <b>EVENTO: </b> <?php echo $EVENTO; ?> <br>
+                        <b>FECHA: </b> <?php echo $FECHA; ?> <br>
+                        <b>HORA_RECEPCION: </b> <?php echo $HORA_RECEPCION; ?><br>
+                        <b>HORA_ATENCION: </b> <?php echo $HORA_ATENCION; ?> <br>
+                        <b>DIRECCION: </b> <?php echo $DIRECCION; ?> <br>
+                        <b>DESCRIPCION: </b> <?php echo $DESCRIPCION; ?><br>
+                        <b>ENTIDAD: </b> <?php echo $ENTIDAD; ?> <br>
+                        <b>OPERADOR_RESPONSABLE: </b> <?php echo $OPERADOR_RESPONSABLE; ?> <br>
+                    </p>
+                </div>
+            </div>
+     
